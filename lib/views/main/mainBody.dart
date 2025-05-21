307940889ea533b5a407729dba4612b39f9efe52
zhang <zjing9105@gmail.com>
2025-05-21 18:26:53 +0800

    add ui

--- lib/views/main/mainBody.dart
+++ lib/views/main/mainBody.dart
@@ -8,26 +8,29 @@ class Mainbody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient:
-            ThemeUtil.isDarkMode(context)
-                ? null
-                : LinearGradient(
-                  begin: Alignment.topCenter, // 或者 Alignment.topLeft
-                  end: Alignment.bottomCenter, // 或者 Alignment.bottomRight
-                  colors: [
-                    Color(0xFFE6E6E9), // 开始颜色
-                    Color(0xFFEAE6DB), // 结束颜色
-                  ],
-                ),
-      ),
-      child: PageView(
-        scrollDirection: Axis.vertical,
-        // 禁止滑动
-        controller: mainctl.pageController,
-        physics: const NeverScrollableScrollPhysics(),
-        children: mainctl.views,
+    return Material(
+      type: MaterialType.transparency,
+      child: Ink(
+        decoration: BoxDecoration(
+          gradient:
+              ThemeUtil.isDarkMode(context)
+                  ? null
+                  : LinearGradient(
+                    begin: Alignment.topCenter, // 或者 Alignment.topLeft
+                    end: Alignment.bottomCenter, // 或者 Alignment.bottomRight
+                    colors: [
+                      Color(0xFFE6E6E9), // 开始颜色
+                      Color(0xFFEAE6DB), // 结束颜色
+                    ],
+                  ),
+        ),
+        child: PageView(
+          scrollDirection: Axis.vertical,
+          // 禁止滑动
+          controller: mainctl.pageController,
+          physics: const NeverScrollableScrollPhysics(),
+          children: mainctl.views,
+        ),
       ),
     );
   }
